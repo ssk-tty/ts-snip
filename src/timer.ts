564b76672fccd2c01e18dd9b66056928c4f1f98c
tatsuya sasaki <sasaki@neopa.jp>
2022-03-04 12:57:48 +0900

    fix timer

--- src/timer.ts
+++ src/timer.ts
@@ -2,10 +2,11 @@ export class Timer {
   /**
    * await ms
    */
-  static sleep = (ms: number): Promise<void> => new Promise((resolve) =>
-    setTimeout(
-      () => {resolve()},
-      ms,
-    )
-  );
+  static sleep = (ms: number): Promise<void> => new Promise(this.doNothingFor(ms));
+
+  /**
+   * Promise executor
+   */
+  private static doNothingFor = (ms: number) =>
+      (resolve: () => void) => setTimeout(() => resolve(), ms);
 }
